04d513066990b5c854a0b73cf02bccc8b8884054
Elliott Hughes-Taylor <ehughestaylor@Elliotts-MacBook-Pro.local>
2015-08-10 11:41:57 -0400

    added portfolio section

--- app/js/main.js
+++ app/js/main.js
@@ -27,10 +27,51 @@
     }
 
     $(document).ready(function(){
+      $(".button-collapse").sideNav();
+      $('.modal-trigger').leanModal();
+      $('.button-collapse').on('mouseenter',function(){
+        $('.fa-circle-thin').removeClass('fa-circle-thin').addClass('fa-circle');
+      });
+      $('.button-collapse').on('mouseleave',function(){
+        $('.fa-circle').removeClass('fa-circle').addClass('fa-circle-thin');
+      });
       $('.slider').slider({
         full_width: true,
         indicators:false
       });
       goToSection();
+      
+      $(".typed-statement").typed({
+        strings: ["<p class='typed-words'>Programmer.</p>", "<p class='typed-words'>Data Manager.</p>", "<p class='typed-words'>Problem Solver.</p>", "<p class='typed-words'>Backend-Developer.</p>"],
+        typeSpeed: 10,
+            // typing speed
+            typeSpeed: 0,
+            // time before typing starts
+            startDelay: 0,
+            // backspacing speed
+            backSpeed: 0,
+            // time before backspacing
+            backDelay: 500,
+            // loop
+            loop: true,
+            // false = infinite
+            loopCount: false,
+            // show cursor
+            showCursor: true,
+            // character for cursor
+            cursorChar: "|",
+            // attribute to type (null == text)
+            attr: null,
+            // either html or text
+            contentType: 'html',
+            // call when done callback function
+            callback: function() {},
+            // starting callback function before each string
+            preStringTyped: function() {},
+            //callback for every typed string
+            onStringTyped: function() {},
+            // callback for reset
+            resetCallback: function() {}
+      });
     });
         
